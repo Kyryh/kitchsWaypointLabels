de4f9cd0fcb6e219afd3e6180c3aec444b0b21a3
kitch <kitchsemail@gmail.com>
2024-01-06 15:55:59 -0600

    Initial Commit - Version 0.1.0

---
+++ kitchsWaypointLabeler/KitchsPatches.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static kitchsWaypointLabeler.KitchsWaypointSettings;
+using Vintagestory.API.Client;
+
+namespace kitchsWaypointLabeler
+{
+    public class KitchsPatches
+    {
+        public static bool Prefix_autoSuggestName(Vintagestory.GameContent.GuiDialogAddWayPoint __instance, ref string ___curIcon, ref string ___curColor)
+        {            
+            WaypointSettings settings = KitchsWaypointSettings.GetSettings(kitchsWaypointLabelerModSystem.CoreApi);
+
+            if( settings == null) { return true; }  // let it be Copper, or ore, or whatever
+
+            string name = settings.Get(___curIcon, ___curColor);
+
+            if( string.IsNullOrEmpty(name) ) { return true; } // let it be Copper, or ore, or whatever
+
+            //kitchsWaypointLabelerModSystem.CoreApi.Logger.Log(Vintagestory.API.Common.EnumLogType.Notification, string.Concat("kitch-autoSuggest-", name, "-", ___curIcon, "-", ___curColor));
+            GuiDialog dlg = (GuiDialog)__instance;
+            GuiElementTextInput textElem = dlg.SingleComposer.GetTextInput("nameInput");
+            textElem.SetValue(name, true);
+            return false;
+        }
+
+        public static void Postfix_onSave(Vintagestory.GameContent.GuiDialogAddWayPoint __instance, ref string ___curIcon, ref string ___curColor)
+        {
+            GuiDialog dlg = (GuiDialog)__instance;
+            string name = dlg.SingleComposer.GetTextInput("nameInput").GetText();
+            kitchsWaypointLabelerModSystem.CoreApi.Logger.Log(Vintagestory.API.Common.EnumLogType.Notification, string.Concat("kitch-onSave-", name, "-", ___curIcon, "-", ___curColor));
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                WaypointSettings settings = KitchsWaypointSettings.GetSettings(kitchsWaypointLabelerModSystem.CoreApi);
+
+                if (settings != null && settings.Set(___curIcon, ___curColor, name)) // will return true, only if something updated.
+                {
+                    KitchsWaypointSettings.SaveSettings(kitchsWaypointLabelerModSystem.CoreApi, settings);
+                }
+            }
+        }
+    }
+}
---
+++ kitchsWaypointLabeler/KitchsWaypointSettings.cs
@@ -0,0 +1,98 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using Vintagestory.API.Common;
+
+namespace kitchsWaypointLabeler
+{
+    public static class KitchsWaypointSettings
+    {
+        public const string ConfigName = "kitchs-waypoint-labels-config.json";
+        public const string ModLabel = "[kitchs-waypoint-labels]";
+
+        public static WaypointSettings cachedClientSettings = null;
+
+        [ProtoContract]
+        public class WaypointSettings
+        {
+            [ProtoMember(1)]
+            public Dictionary<string, string> NameCache { get; set; }
+
+
+            public WaypointSettings()
+            {
+                NameCache = new Dictionary<string, string>();
+            }
+
+            public string Get(string icon, string color)
+            {
+                string nameKey = string.Concat(icon, "-", color);
+                if (NameCache.TryGetValue(nameKey, out string label))
+                {
+                    return label;
+                }
+                return "";
+            }
+
+            public bool Set(string icon, string color, string label)
+            {
+                string nameKey = string.Concat(icon, "-", color);
+
+                if (NameCache.ContainsKey(nameKey))
+                {
+                    if (NameCache[nameKey] != label)
+                    {
+                        NameCache[nameKey] = label;
+                        return true;
+                    }
+                }
+                else
+                {
+                    NameCache.Add(nameKey, label);
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public static WaypointSettings GetSettings(ICoreAPI api)
+        {
+            if (api.Side == EnumAppSide.Server || api.Side == EnumAppSide.Universal)
+            {
+                return null;
+            }
+
+            if (cachedClientSettings != null) { return cachedClientSettings; }
+            WaypointSettings settings = null;
+
+            try
+            {
+                settings = api.LoadModConfig<WaypointSettings>(ConfigName);
+                if (api.Side == EnumAppSide.Client)
+                    cachedClientSettings = settings;
+            }
+            catch
+            {
+                settings = null;
+                api.Logger.Log(EnumLogType.Warning, string.Concat(ModLabel, " Unable to load config file '", ConfigName, "'.  Creating you a fresh new one."));
+            }
+
+            if (settings == null)
+            {
+                settings = new WaypointSettings();
+                SaveSettings(api, settings);
+            }
+            return settings;
+        }
+
+        public static void SaveSettings(ICoreAPI api, WaypointSettings settings)
+        {
+            if (api.Side == EnumAppSide.Server || api.Side == EnumAppSide.Universal)
+            {
+                return;
+            }
+            cachedClientSettings = settings;
+            api.StoreModConfig<WaypointSettings>(settings, ConfigName);
+        }
+    }
+}
