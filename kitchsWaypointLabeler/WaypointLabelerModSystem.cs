46f89f8b4e336d45d329ee018d1182a5ad35ed18
kitch <kitchsemail@gmail.com>
2024-01-20 10:57:25 -0600

    Updated to compile against 1.19.  Added dispose method that I left out.  Added icon image

--- kitchsWaypointLabeler/WaypointLabelerModSystem.cs
+++ kitchsWaypointLabeler/WaypointLabelerModSystem.cs
@@ -1,21 +1,19 @@
 ﻿using HarmonyLib;
-using System.Formats.Asn1;
 using System.Reflection;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
-using Vintagestory.API.Config;
 using Vintagestory.API.Server;
-using static kitchsWaypointLabeler.WaypointConfig;
 
 [assembly: ModInfo("Kitch's Waypoint Labeler", "kitchwaypointlabeler",
                     Authors = new string[] { "kitch" },
                     Description = "A mod that remembers previously used waypoint labels.",
-                    Version = "1.0.0")]
+                    Version = "1.1.0")]
 
 namespace kitchsWaypointLabeler
 {
     public class WaypointLabelerModSystem : ModSystem
     {
+        public const string harmonyId = "kitchwaypointlabeler";
         public static ICoreAPI CoreApi;
         public Harmony harmony;
 
@@ -30,7 +28,7 @@ namespace kitchsWaypointLabeler
         {
             api.Logger.Notification(string.Concat(WaypointConfig.ModLabel, " Loading Kitch's Waypoint Labeler"));
             CoreApi = api;
-            harmony = new Harmony("kitchwaypointlabeler");
+            harmony = new Harmony(harmonyId);
 
             // Pulling the methods via reflection first, just to make sure they are there.
             // The idea here is to prevent this mod from crashing everything if the functions change or are removed.  I have no idea if this works.... we'll see. 
@@ -63,6 +61,12 @@ namespace kitchsWaypointLabeler
                     api.Logger.Log(EnumLogType.Error, string.Concat(WaypointConfig.ModLabel, " Couldn't Find GuiDialogAddWayPoint.onSave"));
                 }
             }           
+        }
+
+        public override void Dispose()
+        {
+            harmony.UnpatchAll(harmonyId);
+            base.Dispose();
         }
     }
 }
